7a62c829c4a58f48bc882f28ad5c2da7134bd08e
Rola Alasmar <rola.alasmar@perficient.com>
2023-08-10 14:41:35 -0500

    React practice meme generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,14 +3,26 @@ import memesData from "../memesData.js"
 
 export default function Meme(){
     //create a new state called "memeImage" with an empty string as default
-    const [memeImage, setMemeImage] = React.useState("")
+    //update our state to save the meme-related data as an object called 'meme' which these 3 properties: topText, bottomText, randomImage
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
     
     //function to generate a random meme
     function getMemeImage(){
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         // when function is called, update the 'memeImage' state to be the random chosen image URL
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage:url
+        }))
     }
 
     return (
@@ -31,7 +43,7 @@ export default function Meme(){
                 onClick={getMemeImage}
                 >Get a new meme image</button>
             </div>
-            <img src={memeImage} className="meme--image" />
+            <img src={meme.randomImage} className="meme--image" />
         </main>
     )
 }
